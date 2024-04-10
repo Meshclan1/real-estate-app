7d4db7ad5ecb280681b14204ec03fcf9ed670ba9
Ayo <Ayo.meshe@gmail.com>
2024-04-11 00:03:46 +0100

    production mode

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -63,7 +63,7 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        // secure:true,
+        secure: true,
         maxAge: age,
       })
       .status(200)
